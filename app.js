376be084d753ecc06f401ec5b1d7ca82cc316f26
DarioQ5 <119014051+DarioQ5@users.noreply.github.com>
2023-08-02 22:08:37 -0300

    Update app.js para render

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const path = require("path");
 //app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(express.static("public"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
-app.listen( PORT, () => console.log(`Server up: PORT:${PORT}` ));
+app.listen( PORT, () => console.log(`Server corriendo en el puerto${PORT}`) );
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
