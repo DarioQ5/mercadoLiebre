b1c3c9661a7ba4868e71e6b4d13eff591bb982c2
Dario Quispe <darioquispe952@gmail.com>
2023-08-06 22:18:44 -0300

    Formulario y estilos

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 })
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
 app.get("/product", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/product.html"))
 })
